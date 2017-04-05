b0ec9aa3abedc3741c9982c807d1ffd5fe505959
Danny <dannydejong@rocketmail.com>
2017-04-05 10:35:24 +0200

    2nd push

--- src/designpaint/DesignPaint.java
+++ src/designpaint/DesignPaint.java
@@ -5,79 +5,16 @@
  */
 package designpaint;
 
-import javax.swing.SwingUtilities;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseMotionAdapter;
-
 
 /**
  *
  * @author danny
  */
 public class DesignPaint {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
+        
     }
-    
-    private static void createAndShowGUI() {
-        System.out.println("Created GUI on EDT? "+
-                SwingUtilities.isEventDispatchThread());
-        JFrame f = new JFrame("Swing Paint Demo");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(250,250);
-        f.add(new MyPanel());
-        f.pack();
-        f.setVisible(true);
-    }
-    
-}
-
-class MyPanel extends JPanel {
-    private int squareX = 50;
-    private int squareY = 50;
-    private int squareW = 20;
-    private int squareH = 20;
-
-    public MyPanel() {
-        setBorder(BorderFactory.createLineBorder(Color.black));
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
-            }
-        });
-
-        addMouseMotionListener(new MouseAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
-            }
-        });
-    }
-
-    public Dimension getPreferredSize() {
-        return new Dimension(250,200);
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);       
-
-        // Draw Text
-        g.drawString("Design Patterns komt hier!",10,20);
-    }  
 }
