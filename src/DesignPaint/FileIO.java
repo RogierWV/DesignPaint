82bb183de6590a57a348d9b89af86d948dfa3af2
Danny <dannydejong@rocketmail.com>
2017-04-10 12:01:49 +0200

    FileIO update

--- src/DesignPaint/FileIO.java
+++ src/DesignPaint/FileIO.java
@@ -1,6 +1,8 @@
-package DesignPaint;
+package designpaint;
 
-import designpaint.Shape;
+//import designpaint.Shape;
+//import designpaint.Ellipse;
+//import designpaint.Rectangle;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,6 +10,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 public class FileIO {
     public static void save(List shapes, String name) {
@@ -24,6 +27,34 @@ public class FileIO {
     }
     
     public static List<Shape> load(String name) {
-        return null;
+        Path path = FileSystems.getDefault().getPath(name);
+        List<Shape> shapes = new ArrayList();
+        int id = 0;
+        try {
+            List<String> lines = Files.readAllLines(path);
+
+            for (String line : lines) {
+              //System.out.println(line);
+              String[] split = line.split(" ");
+              switch(split[0]){
+                  case "ellipse":
+                      shapes.add(new Ellipse(id, Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3]), Integer.parseInt(split[4])));
+                      break;
+                  case "rectangle" :
+                      shapes.add(new Rectangle(id, Integer.parseInt(split[1]), Integer.parseInt(split[2]), Integer.parseInt(split[3]), Integer.parseInt(split[4])));
+                      break;
+                  default:
+                      id--;
+              }
+              id++;
+            }
+          } catch (IOException e) {
+            System.out.println(e);
+          }
+        
+        
+        return shapes;
     }
+    
+    
 }
