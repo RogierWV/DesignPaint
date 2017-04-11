a1ba653168c19a8e8928b63c883a4e3187121bc9
Rogier Velting <rogiervelting@gmail.com>
2017-04-11 13:16:26 +0200

    Javadoc

--- src/designpaint/Select.java
+++ src/designpaint/Select.java
@@ -6,12 +6,28 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 
+/**
+ * Selected shape, used to draw an indicator around the selected shape.
+ */
 public class Select extends Shape {
 
+    /**
+     * Creates a new Select shape.
+     * @param id The id of the shape.
+     * @param coordinateX The X coordinate of the origin of the shape.
+     * @param coordinateY The Y coordinate of the origin of the shape.
+     * @param width The width of the shape.
+     * @param height Height of the shape.
+     */
     public Select(int id, int coordinateX, int coordinateY, int width, int height) {
         super(id, coordinateX, coordinateY, width, height);
     }
 
+    /**
+     * Draws the shape at its coordinates.
+     * @param graphics Graphics generator
+     * @return this (for mapping)
+     */
     @Override
     public Shape draw(Graphics graphics) {
         Graphics2D g2 = (Graphics2D) graphics;
@@ -25,11 +41,19 @@ public class Select extends Shape {
         return this;
     }
     
+    /**
+     * Gets the name of the Shape subclass used to instantiate this.
+     * @return The name of the Shape type (presumably Select)
+     */
     @Override
     public String getShapeType(){
         return this.getClass().getSimpleName();
     }
 
+    /**
+     * Generates a String representation of this Shape, that can be saved to a file.
+     * @return String representation of this Shape
+     */
     @Override
     public String toString() {
         return "select " + coordinateX + " " + coordinateY + " " + width + " " + height;
