b8d502ca5b59d1d2b158ddfcfe5dde2b2591605b
Mattuh\Matthe <m_jacobs@hotmail.nl>
2017-04-19 10:19:59 +0200

    command load en save, en visitor

---
+++ src/designpaint/SaveVisitor.java
@@ -0,0 +1,19 @@
+package designpaint;
+
+/**
+ *
+ * @author Matthe
+ */
+public class SaveVisitor implements Visitor{
+
+    @Override
+    public void Visit(Component component) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Visit(Composite composite) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
